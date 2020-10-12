282fe8e0486f60012b936e7c117f66aa457f5d37
Jekkow <50441604+Jekkow@users.noreply.github.com>
2020-10-12 20:32:18 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,38 @@
+import pygame
+from halma.constants import *
+from halma.board import Board
+from halma.game import Game
+
+FPS = 60
+WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Halma")
+
+def get_row_col_from_mouse(pos):
+    x, y = pos
+    row = y // SQAURE_SIZE
+    col = x // SQAURE_SIZE
+    return row, col
+
+def main():
+    run = True
+    clock = pygame.time.Clock()
+    game = Game(WINDOW)
+
+    while run:
+        clock.tick(FPS)
+        
+        if game.winner() != None:
+            print(game.winner())
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pos = pygame.mouse.get_pos()
+                row, col = get_row_col_from_mouse(pos)
+                #if game.turn == GREEN:
+                game.select(row, col)
+
+        game.update()
+    pygame.quit()
+main()
